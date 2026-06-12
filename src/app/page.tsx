433513f2ab37a5b6560f8c8ebd9bb42fc0d12b4d
Henrincode <spellctro@gmail.com>
2026-06-12 01:22:31 -0300

    cleanup: cleaning up sample code from the nextjs framework installation

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="
+    flex justify-center items-center
+    h-dvh w-dvw
+    text-7xl
+    text-indigo-300
+    bg-indigo-950
+    ">
+      <h1>Olá mundo!</h1>
+    </div>
+  );
+}
